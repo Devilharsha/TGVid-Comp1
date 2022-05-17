e36001e6b242f8b408cdef892da58dfbce486e8e
Shirou <76389937+Subhakantadas@users.noreply.github.com>
2022-05-17 08:04:02 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 #    Copyright (c) 2021 Danish_00
-#    Improved By @Zylern
+#    Improved By @Shirou
 
 from decouple import config
 
@@ -7,10 +7,10 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1664850827
+    DEV = 5211097098
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    ffmpegcode = ["-preset fast -c:v libx265 -s 1280:-2 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Shirou' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 128k -vbr on -c:s copy -map 0 threads 1"]
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/ebde50b591e0e6c19366c.png")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
