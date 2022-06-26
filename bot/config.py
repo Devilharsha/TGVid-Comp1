4c68a7d323a34bae27068679d6b1a506092f1a05
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2022-06-27 02:16:01 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 5211097098
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset fast -c:v libx265 -s 1280:-2 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Shirou' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 128k -vbr on -c:s copy -map 0 threads 1"]
+    ffmpegcode = ["-preset veryfast -c:v libx265 -s 1280:-2 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By DaddyCooL' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 128k -vbr on -c:s copy -map 0 threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/ebde50b591e0e6c19366c.png")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
